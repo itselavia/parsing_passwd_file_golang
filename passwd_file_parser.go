b3e791bded7ed092d599172f4f90c927e99e4a57
Akshay Elavia <akshay.elavia@sjsu.edu>
2020-03-19 21:58:18 -0700

    Added comments and minor fix

--- passwd_file_parser.go
+++ passwd_file_parser.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-//UserProp - Struct to hold user properties information
+//UserProp - Struct to hold user properties information. Also, specifying the corresponding JSON field names
 type UserProp struct {
 	UID      string   `json:"uid"`
 	FullName string   `json:"full_name"`
@@ -19,12 +19,14 @@ type UserProp struct {
 
 func main() {
 
+	//If more than expected arguments provided, throw an error
 	if len(os.Args) > 3 {
 		log.Fatalf("Usage: ./passwd_file_parser paaswd_file_path groups_file_path")
 	}
 
 	var paaswd_file_path, groups_file_path string
 
+	//If file paths are not provided with arguments, use default LINUX locations
 	if len(os.Args) < 2 {
 
 		paaswd_file_path = "/etc/passwd"
@@ -44,9 +46,11 @@ func main() {
 	check(err)
 	defer groups_file.Close()
 
+	//Initialize map to hold data in the expected format
 	var users = make(map[string]UserProp)
 
 	passwd_reader := bufio.NewReader(passwd_file)
+	//Read line by line
 	for {
 		line, _, err := passwd_reader.ReadLine()
 
@@ -56,11 +60,13 @@ func main() {
 
 		passwd_splits := strings.Split(string(line), ":")
 
+		//Strat populating UID and FullName fields in the map, initialize empty slice for Groups
 		users[passwd_splits[0]] = UserProp{UID: passwd_splits[2], FullName: passwd_splits[4], Groups: make([]string, 0)}
 
 	}
 
 	groups_reader := bufio.NewReader(groups_file)
+	//Read line by line
 	for {
 		line, _, err := groups_reader.ReadLine()
 
@@ -70,15 +76,17 @@ func main() {
 
 		groups_splits := strings.Split(string(line), ":")
 
+		//If last column in the groups file is not empty
 		if len(groups_splits[3]) > 0 {
 
 			usernames := strings.Split(groups_splits[3], ",")
+
+			//For each username in the group, populate the Groups field in the map
 			for _, user := range usernames {
 
-				if thisUser, ok := users[user]; ok {
-					thisUser.Groups = append(thisUser.Groups, groups_splits[0])
-					users[user] = thisUser
-				}
+				thisUser := users[user]
+				thisUser.Groups = append(thisUser.Groups, groups_splits[0])
+				users[user] = thisUser
 
 			}
 
@@ -86,6 +94,7 @@ func main() {
 
 	}
 
+	//Convert map to JSON. Pretty printing with tabs
 	jsonString, err := json.MarshalIndent(users, "", "\t")
 	check(err)
 	fmt.Println(string(jsonString))
